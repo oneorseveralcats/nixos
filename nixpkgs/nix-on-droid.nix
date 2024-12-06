82dd30c21f8b618f314406816dee65da0cf7b42a
. <.>
2024-12-05 20:32:11 -0500

    configure android-integrations

--- nixpkgs/nix-on-droid.nix
+++ nixpkgs/nix-on-droid.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }:
-
 {
-  # Read Nix-on-Droid changelog before changing this value
-  system.stateVersion = "21.11";
+  android-integration = {
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+    xdg-open.enable = true;
+  };
 
   home-manager.config = ../home-manager/home.nix;
+  system.stateVersion = "21.11";
 }
