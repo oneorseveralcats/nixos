5b053594ae33c17d6dfaf2e50df1fecb72efdfaf
. <.>
2024-12-05 20:45:10 -0500

    fix typo (for real this time ._.)

--- home-manager/devices/nix-on-droid.nix
+++ home-manager/devices/nix-on-droid.nix
@@ -4,7 +4,7 @@
   myHome = {
     desktop.enable = false;
     books.enable = false;
-    gaming.gui.enable = false;
+    games.gui.enable = false;
     multimedia.enable = false;
     probation.enable = false;
     rax.enable = false;
