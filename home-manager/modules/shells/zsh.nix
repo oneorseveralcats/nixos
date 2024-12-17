c9811a7c490127ddae3379401f778c1109fe3683
. <.>
2024-12-16 21:13:22 -0500

    zsh: configure

--- home-manager/modules/shells/zsh.nix
+++ home-manager/modules/shells/zsh.nix
@@ -15,6 +15,41 @@ in
   config = mkIf cfg.enable {
     programs.zsh = {
       enable = true;
+      dotDir = ".config/zsh";
+
+      autocd = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      history = {
+        path = "${config.xdg.configHome}/zsh/zsh_history";
+        expireDuplicatesFirst = true;
+      };
+      shellAliases = {
+        zr = "source ${config.xdg.configHome}/zsh/.zshrc";
+      };
+
+      initExtra = ''
+        autoload -U colors && colors 
+
+        if [ -n "$IN_NIX_SHELL" ]; then
+          PS1="%{$fg[green]%}%~%{$reset_color%}> "
+        else 
+          case "$(whoami)" in
+            root) 
+              PS1="%{$fg[red]%}%~%{$reset_color%}> ";;
+            *) 
+              PS1="%{$fg[blue]%}%~%{$reset_color%}> ";;
+          esac
+        fi
+      '';
+
+      plugins = with pkgs; [
+        { name = zsh-autopair.pname; src = zsh-autopair.src; }
+        { name = zsh-completions.pname; src = zsh-completions.src; }
+        { name = zsh-nix-shell.pname; src = zsh-nix-shell.src; }
+        { name = zsh-vi-mode.pname; src = zsh-vi-mode.src; }
+      ];
     };
   };
 }
