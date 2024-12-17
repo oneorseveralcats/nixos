00c040ae37a899ef7c96db2b8e4be5277768d988
. <.>
2024-12-16 22:41:42 -0500

    zsh: add git rprompt

--- home-manager/modules/shells/zsh.nix
+++ home-manager/modules/shells/zsh.nix
@@ -33,20 +33,24 @@ in
         autoload -U colors && colors 
 
         if [ -n "$IN_NIX_SHELL" ]; then
-          PS1="%{$fg[green]%}%~%{$reset_color%}> "
+          PROMPT="%{$fg[green]%}%~%{$reset_color%}> "
         else 
           case "$(whoami)" in
             root) 
-              PS1="%{$fg[red]%}%~%{$reset_color%}> ";;
+              PROMPT="%{$fg[red]%}%~%{$reset_color%}> ";;
             *) 
-              PS1="%{$fg[blue]%}%~%{$reset_color%}> ";;
+              PROMPT="%{$fg[blue]%}%~%{$reset_color%}> ";;
           esac
         fi
+
+        setopt promptsubst
+        RPROMPT="\$(${pkgs.gitprompt-rs}/bin/gitprompt-rs zsh)"
       '';
 
       plugins = with pkgs; [
         { name = zsh-autopair.pname; src = zsh-autopair.src; }
         { name = zsh-completions.pname; src = zsh-completions.src; }
+        { name = zsh-forgit.pname; src = zsh-forgit.src; file = "forgit.plugin.zsh"; }
         { name = zsh-nix-shell.pname; src = zsh-nix-shell.src; }
         { name = zsh-vi-mode.pname; src = zsh-vi-mode.src; }
       ];
