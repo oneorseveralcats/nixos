3222bf0ca9e2263ece7a78fa1748a1ca9ed59205
. <.>
2024-12-17 05:31:51 -0500

    helix: move to editors/helix.nix

--- home-manager/modules/base.nix
+++ home-manager/modules/base.nix
@@ -248,65 +248,6 @@ in
       };
     };
 
-    programs.helix = {
-      enable = true;
-      defaultEditor = true;
-      settings = {
-        theme = "base16_transparent";
-        editor = {
-          bufferline = "multiple";
-          color-modes = true;
-          mouse = false;
-          lsp.display-messages = true;
-          soft-wrap.enable = true;
-          auto-pairs = {
-            "(" = ")";
-            "{" = "}";
-            "[" = "]";
-            "\"" = "\"";
-          };
-          cursor-shape = {
-            insert = "bar";
-            normal = "block";
-            select = "underline";
-          };
-          indent-guides = {
-            render = true;
-            character = "╎";
-            skip-levels = 1;
-          };
-          statusline = {
-            left = [ "mode" "spinner" "file-name" "read-only-indicator" "file-modification-indicator" ];
-            center = [ "file-type" ];
-            right = [ "diagnostics" "spacer" "selections" "spacer" "position-percentage" "spacer" "position" "spacer" "register" ];
-            mode = {
-              normal = "NORMAL";
-              insert = "INSERT";
-              select = "SELECT";
-            };
-          };
-        };
-        keys.normal = {
-          Z.Z = [ ":wqa!" ];
-          g.t = [ ":buffer-next" ];
-          g.T = [ ":buffer-previous" ];
-          X = [ "extend_line_up"  "extend_to_line_bounds" ];
-        };
-        keys.select = {
-          X = [ "extend_line_up"  "extend_to_line_bounds" ];
-        };
-      };
-      languages = {
-        # language-server.jdtls = {
-        #   command = "${pkgs.jdt-language-server}/bin/jdt-language-server";
-        # };
-      };
-      extraPackages = with pkgs; lib.mkDefault [
-        marksman
-        nil nodePackages.bash-language-server
-      ];
-    };
-
     programs.pandoc = {
       enable = true;
       defaults = {
