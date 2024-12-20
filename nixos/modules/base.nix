0758a01c9695fe4fabe87c52f4aa9229f229a065
oneorseveralcats <oneorseveralcats@noreply.codeberg.org>
2024-12-20 03:58:11 -0500

    fontconfig: disable (conflicts with hm fontconfig)

--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -123,27 +123,27 @@ in
       ];
     };
   
-    fonts = {
-      packages = with pkgs; [
-        corefonts
-        fira-code-nerdfont
-        mno16
-        noto-fonts
-        noto-fonts-cjk-sans
-        spleen
-        twemoji-color-font
-      ];
-      fontDir.enable = true;
-      enableDefaultPackages = true;
-      fontconfig = {
-        defaultFonts = {
-          serif = [ "Noto Serif Light" "Noto Serif" ];
-          sansSerif = [ "Noto Sans Light" "Noto Sans" ];
-          monospace = [ "Fira Code Nerd Font Light" "Fira Code Light" "Noto Sans Mono" ];
-          emoji = [ "Twitter Color Emoji" ];
-        };
-      };
-    };
+    # fonts = {
+    #   packages = with pkgs; [
+    #     corefonts
+    #     fira-code-nerdfont
+    #     mno16
+    #     noto-fonts
+    #     noto-fonts-cjk-sans
+    #     spleen
+    #     twemoji-color-font
+    #   ];
+    #   fontDir.enable = true;
+    #   enableDefaultPackages = true;
+    #   fontconfig = {
+    #     defaultFonts = {
+    #       serif = [ "Noto Serif Light" "Noto Serif" ];
+    #       sansSerif = [ "Noto Sans Light" "Noto Sans" ];
+    #       monospace = [ "Fira Code Nerd Font Light" "Fira Code Light" "Noto Sans Mono" ];
+    #       emoji = [ "Twitter Color Emoji" ];
+    #     };
+    #   };
+    # };
 
     i18n.defaultLocale = "en_US.UTF-8";
     services.xserver.xkb.layout = "us,us";
