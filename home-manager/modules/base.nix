7e7303996062ae643af6c52469adaa110db47808
oneorseveralcats <oneorseveralcats@noreply.codeberg.org>
2024-12-22 19:03:47 -0500

    mpv: no auto window resize

--- home-manager/modules/base.nix
+++ home-manager/modules/base.nix
@@ -202,6 +202,7 @@ in
       };
       config = {
         af = "scaletempo2=max-speed=10";
+        auto-window-resize= "no";
         osc = "no";
         osd-font-size = "20";
         osd-level = "3";
