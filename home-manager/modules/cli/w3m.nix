61dcd6405ed9053178979378589c0b3dced1a9e1
oneorseveralcats <oneorseveralcats@noreply.codeberg.org>
2024-12-26 23:06:32 -0500

    w3m: add session variables

--- home-manager/modules/cli/w3m.nix
+++ home-manager/modules/cli/w3m.nix
@@ -22,7 +22,8 @@ in
       w3m = "w3m -sixel";
     };
 
-    home.sessionVariables = {
+    home.sessionVariables = rec {
+      WWW_HOME = "${W3M_DIR}/bookmark.html";
       W3M_DIR = "${config.xdg.configHome}/w3m";
       W3M_IMG2SIXEL = "img2sixel -d atkinson";
     };
