30f1d37685cca9b525837a80038fb9938f11c0ef
oneorseveralcats <oneorseveralcats@noreply.codeberg.org>
2024-12-26 23:08:51 -0500

    stylix: change theme to evenok-dark

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -26,26 +26,27 @@ in
       polarity = "dark";
       image = lib.mkDefault "${pkgs.lxqt.lxqt-themes}/share/lxqt/wallpapers/origami-dark.png";
       imageScalingMode = lib.mkDefault "fit";
-      base16Scheme = {
-        name = "OneDark Dark";
-        author = "olimorris (https://github.com/olimorris)";
-        base00 = "000000";  
-        base01 = "1c1f24";  
-        base02 = "2c313a";  
-        base03 = "434852";  
-        base04 = "565c64";  
-        base05 = "abb2bf";  
-        base06 = "b6bdca";  
-        base07 = "c8ccd4";  
-        base08 = "ef596f";  
-        base09 = "d19a66";  
-        base0A = "e5c07b";  
-        base0B = "89ca78";  
-        base0C = "2bbac5";  
-        base0D = "61afef";  
-        base0E = "d55fde";  
-        base0F = "be5046";  
-      };
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/evenok-dark.yaml";
+      # base16Scheme = {
+      #   name = "OneDark Dark";
+      #   author = "olimorris (https://github.com/olimorris)";
+      #   base00 = "000000";  
+      #   base01 = "1c1f24";  
+      #   base02 = "2c313a";  
+      #   base03 = "434852";  
+      #   base04 = "565c64";  
+      #   base05 = "abb2bf";  
+      #   base06 = "b6bdca";  
+      #   base07 = "c8ccd4";  
+      #   base08 = "ef596f";  
+      #   base09 = "d19a66";  
+      #   base0A = "e5c07b";  
+      #   base0B = "89ca78";  
+      #   base0C = "2bbac5";  
+      #   base0D = "61afef";  
+      #   base0E = "d55fde";  
+      #   base0F = "be5046";  
+      # };
       
       cursor = {
         name = "phinger-cursors-dark";
