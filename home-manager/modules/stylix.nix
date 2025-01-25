9b35569f652fd1bce4b3e8dbbd37f8b296bc89d0
oneorseveralcats <oneorseveralcats@noreply.codeberg.org>
2025-01-25 11:26:51 -0500

    stylix: clean up helix theme overrides

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -182,99 +182,18 @@ in
       }
     '';
 
-    stylix.targets.helix.enable = true;
-    programs.helix.settings.theme = lib.mkForce "stylix-custom";
-    programs.helix.themes = {
-      stylix-custom = let
-        base00 = "#${config.lib.stylix.colors.base00-hex}";
-        base01 = "#${config.lib.stylix.colors.base01-hex}";
-        base02 = "#${config.lib.stylix.colors.base02-hex}";
-        base03 = "#${config.lib.stylix.colors.base03-hex}";
-        base04 = "#${config.lib.stylix.colors.base04-hex}";
-        base05 = "#${config.lib.stylix.colors.base05-hex}";
-        base06 = "#${config.lib.stylix.colors.base06-hex}";
-        base07 = "#${config.lib.stylix.colors.base07-hex}";
-        base08 = "#${config.lib.stylix.colors.base08-hex}";
-        base09 = "#${config.lib.stylix.colors.base09-hex}";
-        base0A = "#${config.lib.stylix.colors.base0A-hex}";
-        base0B = "#${config.lib.stylix.colors.base0B-hex}";
-        base0C = "#${config.lib.stylix.colors.base0C-hex}";
-        base0D = "#${config.lib.stylix.colors.base0D-hex}";
-        base0E = "#${config.lib.stylix.colors.base0E-hex}";
-        base0F = "#${config.lib.stylix.colors.base0F-hex}";
-      in {
-        "attributes" = base09;
-        "comment" = { fg = base03; modifiers = ["italic"]; };
-        "constant" = base09;
-        "constant.character.escape" = base0C;
-        "constant.numeric" = base09;
-        "constructor" = base0D;
-        "debug" = base03;
-        "diagnostic" = { underline.style = "line"; };
-        "diff.delta" = base09;
-        "diff.minus" = base08;
-        "diff.plus" = base0B;
-        "error" = base08;
-        "function" = base0D;
-        "hint" = base03;
-        "info" = base0D;
-        "keyword" = base0E;
-        "label" = base0E;
-        "namespace" = base0E;
-        "operator" = base05;
-        "special" = base0D;
-        "string"  = base0B;
-        "type" = base0A;
-        "variable" = base08;
-        "variable.other.member" = base0B;
-        "warning" = base09;
+    programs.helix = {
+      settings.theme = lib.mkForce "stylix-custom";
+      themes = {
+        stylix-custom = {
+          inherits = "stylix";
 
-        "markup.bold" = { fg = base0A; modifiers = ["bold"]; };
-        "markup.heading" = base0D;
-        "markup.italic" = { fg = base0E; modifiers = ["italic"]; };
-        "markup.link.text" = base08;
-        "markup.link.url" = { fg = base09; underline.style = "line"; };
-        "markup.list" = base08;
-        "markup.quote" = base0C;
-        "markup.raw" = base0B;
-        "markup.strikethrough" = { modifiers = ["crossed_out"]; };
-
-        "diagnostic.hint" = { underline = { style = "curl"; }; };
-        "diagnostic.info" = { underline = { style = "curl"; }; };
-        "diagnostic.warning" = { underline = { style = "curl"; }; };
-        "diagnostic.error" = { underline = { style = "curl"; }; };
-
-        "ui.background" = { bg = base00; };
-        "ui.bufferline.active" = { fg = base00; bg = base0D; modifiers = ["bold"]; }; # Edited
-        "ui.bufferline" = { fg = base04; bg = base00; };
-        "ui.cursor" = { fg = base0A; modifiers = ["reversed"]; };
-        "ui.cursor.insert" = { fg = base0A; modifiers = ["reversed"]; };
-        "ui.cursorline.primary" = { fg = base05; bg = base01; };
-        "ui.cursor.match" = { fg = base0A; underline.style = "line"; };
-        "ui.cursor.primary" = { fg = base0A; modifiers = ["reversed"]; }; # Added
-        "ui.cursor.select" = { fg = base0A; modifiers = ["reversed"]; };
-        "ui.gutter" = { bg = base00; };
-        "ui.help" = { fg = base06; bg = base01; };
-        "ui.linenr" = { fg = base03; bg = base00; };
-        "ui.linenr.selected" = { fg = base04; bg = base01; modifiers = ["bold"]; };
-        "ui.menu" = { fg = base05; bg = base01; };
-        "ui.menu.scroll" = { fg = base03; bg = base01; };
-        "ui.menu.selected" = { fg = base01; bg = base04; };
-        "ui.popup" = { bg = base01; };
-        "ui.selection" = { bg = base02; };
-        "ui.selection.primary" = { bg = base02; };
-        "ui.statusline" = { fg = base04; bg = base01; };
-        "ui.statusline.inactive" = { bg = base01; fg = base03; };
-        "ui.statusline.insert" = { fg = base00; bg = base0B; };
-        "ui.statusline.normal" = { fg = base00; bg = base0D; }; # Modified
-        "ui.statusline.select" = { fg = base00; bg = base0F; };
-        "ui.text" = base05;
-        "ui.text.focus" = base05;
-        "ui.virtual.indent-guide" = { fg = base03; };
-        "ui.virtual.inlay-hint" = { fg = base03; };
-        "ui.virtual.ruler" = { bg = base01; };
-        "ui.virtual.jump-label" = { fg = base0A; modifiers = ["bold"]; };
-        "ui.window" = { bg = base01; };
+          "ui.statusline.normal" = { fg = "base00"; bg = "base0D"; };
+          "ui.bufferline.active" = { fg = "base00"; bg = "base0D"; modifiers = ["bold"]; };
+          "ui.cursor.primary" = { fg = "base0D"; modifiers = ["reversed"]; };
+          "ui.cursor.match" = { fg = "base0D"; underline.style = "line"; };
+          # "ui.cursor.select" = { fg = "base0A"; modifiers = ["reversed"]; };
+        };
       };
     };
 
