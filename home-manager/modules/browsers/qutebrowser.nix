dab0870b9bab5cd3b2e0890891cb06ff51758c83
oneorseveralcats <oneorseveralcats@noreply.codeberg.org>
2025-02-22 20:11:44 -0500

    qutebrowser: do some configuration

--- home-manager/modules/browsers/qutebrowser.nix
+++ home-manager/modules/browsers/qutebrowser.nix
@@ -11,6 +11,24 @@ in
   config = mkIf cfg.enable {
     programs.qutebrowser = {
       enable = true;
+      settings = {
+        auto_save.session = true;
+        colors.webpage.darkmode.enabled = false;
+        tabs = {
+          show = "multiple";
+          background = false;
+        };
+      };
+      searchEngines = {
+        aw = "https://wiki.archlinux.org/index.php?search={}";
+        am = "https://man.archlinux.org/search?q={}";
+        np = "https://search.nixos.org/packages?query={}";
+        no = "https://search.nixos.org/options?query={}";
+        nw = "https://wiki.nixos.org/w/index.php?search={}";
+        ha = "https://hackage.haskell.org/package/{}/docs/";
+        yt = "https://www.youtube.com/results?search_query={}";
+        w =  "https://en.wikipedia.org/w/index.php?search={}";
+      };
     };
   };
 }
