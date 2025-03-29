2eda369ba9cd8d5686478cd057cdbc637bcd2912
oneorseveralcats <oosc@noreply.codeberg.org>
2025-03-29 18:17:29 -0400

    profiles: add wayland profile

---
+++ home-manager/profiles/wayland.nix
@@ -0,0 +1,21 @@
+{...}:
+{
+  myHome = {
+    browsers.firefox.enable = true;
+    desktop = {
+      compositors.sway.enable = true;
+      terminals.foot.enable = true;
+
+      base.enable = true;
+      extras.enable = true;
+    };
+    media = {
+      mpv.enable = true;
+      ncmpcpp.enable = true;
+      newsboat.enable = true;
+      zathura.enable = true;
+
+      extras.enable = true;
+    };
+  };
+}
