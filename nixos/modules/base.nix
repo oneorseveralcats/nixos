3aa070c3578eecab129ff1f2fed4b6f4b7bc676e
oneorseveralcats <oosc@noreply.codeberg.org>
2025-04-05 19:04:47 -0400

    nixos: move various configuration blocks to different modules

--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -15,7 +15,6 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       acpi
-      doas-sudo-shim
       entr
       keyd
       ffmpeg
@@ -133,46 +132,11 @@ in
     #   };
     # };
 
-    i18n.defaultLocale = "en_US.UTF-8";
-    services.xserver.xkb.layout = "us,us";
-    services.xserver.xkb.options = "lv3:ralt_switch_multikey,esperanto:dvorak,grp:shifts_toggle";
-    services.xserver.xkb.variant = "dvorak,";
-    console = {
-      font = "Lat2-Terminus16";
-      useXkbConfig = true;
-    };
-
-    security = {
-      sudo.enable = false;
-      doas = {
-        enable = true;
-        extraRules = [
-          { groups = [ "wheel" ]; persist = true; keepEnv = true; }
-        ];
-      };
-    };
-
-    networking = {
-      networkmanager.enable = true;
-      nameservers = [
-        "194.242.2.4" # Mullvad Base
-        "194.242.2.3" # Mullvad Adblocking
-      ];
-      wireless = {
-        secretsFile = "/etc/secrets/wifi";
-        networks = {
-          NETGEAR21.pskRaw = "ext:PSK_HOME";
-        };
-      };
-    };
-
     fileSystems."/media/removable" = {
       device = "//192.168.1.10/removable";
       fsType = "cifs";
       options = [ "_netdev" "defaults" "credentials=/etc/secrets/share_removable" "uid=1000" ];
     };
-
-    networking.firewall.enable = true;
 
     nix = {
       gc = {
@@ -183,43 +147,6 @@ in
       settings.auto-optimise-store = true;
     };
 
-    services.keyd = {
-      enable = true;
-      keyboards = rec {
-        default = {
-          ids = [ "*" ];
-          settings = {
-            global = {
-              overload_tap_timeout = "1000";
-            };
-            main = {
-              "capslock" = "overload(control, esc)";
-              "esc" = "capslock";
-              "rightshift" = "rightshift"; # fixes xbkoption grp:shifts_toggle
-            };
-          };
-        };
-        t470s = {
-          ids = [ "0001:0001:a38e6885" "0001:0001:70533846" ];
-          settings = lib.recursiveUpdate default.settings {
-            main = {
-              "sysrq" = "layer(meta)";
-            };
-          };
-        };
-        matcha = {
-          ids = [ "3151:4011:5b3db59a" ];
-          settings = lib.recursiveUpdate default.settings {
-            main = {
-              "esc" = "`";
-              "S-esc" = "~";
-              "`" = "capslock";
-              "pause" = "delete";
-            };
-          };
-        };
-      };
-    };
 
     security.rtkit.enable = true;
     services.pipewire = {
--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,22 +1,20 @@
 { ... }:
 {
   imports = [
+    ./connections
     ./virtualization
+    ./self-hosted
+    ./socials
+    ./system
+    ./users
 
     ./ai.nix
-    ./antivirus.nix
     ./base.nix
-    ./bluetooth.nix
     ./boot-amd64.nix
     ./college.nix
-    ./flatpak.nix
     ./gaming.nix
-    ./irc.nix
-    ./print_scan.nix
     ./rax.nix
-    ./root.nix
     ./sway.nix
-    ./tablet.nix
   ];
 }
 
