8f881e1bde2f2d79b43bd1d7c38048d737b97d87
oneorseveralcats <oosc@noreply.codeberg.org>
2025-04-17 21:50:17 -0400

    testing: disable crystal/smalltalk modules

--- home-manager/modules/testing.nix
+++ home-manager/modules/testing.nix
@@ -16,9 +16,6 @@ in
     home.packages = with pkgs; [
     ];
 
-    myHome.programming.languages.crystal.enable = true;
-    myHome.programming.languages.smalltalk.enable = true;
-
     myHome.editors.neovim.enable = true;
   };
 }
