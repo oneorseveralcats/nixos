eabc0ee519a4ef03a016b1ba4a8f21ff80086325
oneorseveralcats <oosc@noreply.codeberg.org>
2025-05-02 21:32:49 -0400

    programming: remove default enables

--- home-manager/modules/programming/default.nix
+++ home-manager/modules/programming/default.nix
@@ -38,31 +38,4 @@ in
     ./smalltalk.nix
     ./zig.nix
   ];
-
-  options.myHome.programming = {
-    enable = lib.mkOption {
-      description = "Enable programming languages and tools.";
-      type = types.bool;
-      default = true;
-    };
-  };
-
-  config = mkIf cfg.enable {
-    myHome.programming = {
-      base.enable = lib.mkDefault true;
-
-      languages = {
-        c.enable = lib.mkDefault true;
-        common-lisp.enable = lib.mkDefault true;
-        elm.enable = lib.mkDefault true;
-        haskell.enable = lib.mkDefault true;
-        idris.enable = lib.mkDefault true;
-        lua.enable = lib.mkDefault true;
-        purescript.enable = lib.mkDefault true;
-        python.enable = lib.mkDefault true;
-        scheme.enable = lib.mkDefault true;
-        rust.enable = lib.mkDefault true;
-      };
-    };
-  };
 }
