f0e09c7641f458e6850453e06c4cd5fb78951f27
oneorseveralcats <oosc@noreply.codeberg.org>
2025-05-02 23:40:23 -0400

    zellij: add layout for programming

--- home-manager/modules/cli/zellij.nix
+++ home-manager/modules/cli/zellij.nix
@@ -18,56 +18,49 @@ in
       settings = {
         mouse_mode = false;
         simplified_ui = true;
-        theme = "catppuccin-mocha";
-        themes.custom = with config.home.sessionVariables; {
-          fg = "#${foreground}";
-          bg = "#${background}";
-          black = "#${black}";
-          red = "#${red}";
-          green = "#${green}";
-          yellow = "#${yellow}";
-          blue = "#${blue}";
-          magenta = "#${magenta}";
-          cyan = "#${cyan}";
-          white = "#${white}";
-          orange = "#${brightRed}";
-        };
         ui = {
           pane_frames.hide_session_name = true;
         };
       };
     };
-    home.file.".config/zellij/layouts/multimedia.kdl" = {
-      text = ''
-        layout name="multimedia" {
-        	default_tab_template {
-        		pane name="tab-bar" size=1 borderless=true {
-        	        plugin location="zellij:tab-bar"
-        	    }
-        		children
-        	    pane name="status-bar" size=2 borderless=true {
-        	        plugin location="zellij:status-bar"
-        	    }
-        	}
+    home.file.".config/zellij/layouts/multimedia.kdl".text = ''
+      layout name="multimedia" {
+      	default_tab_template {
+      		pane name="tab-bar" size=1 borderless=true {
+      	        plugin location="zellij:tab-bar"
+      	    }
+      		children
+      	    pane name="status-bar" size=2 borderless=true {
+      	        plugin location="zellij:status-bar"
+      	    }
+      	}
 
-        	tab name="ncmpcpp" {
-        		pane command="ncmpcpp"
-        	}
-        	tab name="newsboat" {
-        		pane command="newsboat"
-        	}
-        	tab name="audio" split_direction="horizontal" {
-        		pane command="pulsemixer"
-        		pane command="bluetuith"
-        	}
-        	tab name="background" {
-        		pane {
-              command "bash"
-              args "-c" "while true; do ~/projects/programming/rssfeed_hackery/run.sh; sleep 1h; done"
-            }
-        	}
+      	tab name="ncmpcpp" {
+      		pane command="ncmpcpp"
+      	}
+      	tab name="newsboat" {
+      		pane command="newsboat"
+      	}
+      	tab name="audio" split_direction="horizontal" {
+      		pane command="pulsemixer"
+      		pane command="bluetuith"
+      	}
+      	tab name="background" {
+      		pane {
+            command "bash"
+            args "-c" "while true; do ~/projects/programming/rssfeed_hackery/run.sh; sleep 1h; done"
+          }
+      	}
+      }
+    '';
+    home.file.".config/zellij/layouts/programming.kdl".text = ''
+      layout name="programming" {
+        tab name="Programming" {
+          pane name="helix" size="80%"
+          pane name="output" size="20%"
+          
         }
-      '';
-    };
+      }
+    '';
   };
 }
