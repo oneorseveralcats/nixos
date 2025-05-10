36cb053ae3ed4cf622921ff9ae6e6ec03b5e8274
oneorseveralcats <oosc@noreply.codeberg.org>
2025-05-10 04:02:15 -0400

    joshuto: setup pistol support (does not support sixel)

--- home-manager/modules/file-managers/joshuto.nix
+++ home-manager/modules/file-managers/joshuto.nix
@@ -24,10 +24,47 @@ in
         preview = {
           preview_protocol = "sixel";
           max_preview_size = "10GB";
-          preview_script = "${pkgs.pistol}/bin/pistol";
+          preview_script = "${config.xdg.configHome}/joshuto/preview";
         };
       };
       keymap = {};
+    };
+
+
+    xdg.configFile."joshuto/preview" = {
+      executable = true;
+      text = ''
+        #!/usr/bin/env bash
+        
+        IFS=$'\n'
+
+        # set -o noclobber -o noglob -o nounset -o pipefail
+
+        # FILE_PATH=""
+        # PREVIEW_WIDTH=10
+        # PREVIEW_HEIGHT=10
+
+        while [ "$#" -gt 0 ]; do
+            case "$1" in
+            "--path")
+                shift
+                FILE_PATH="$1"
+                ;;
+            "--preview-width")
+                shift
+                PREVIEW_WIDTH="$1"
+                ;;
+            "--preview-height")
+                shift
+                PREVIEW_HEIGHT="$1"
+                ;;
+            esac
+            shift
+        done
+
+        # image previews currently don't work because joshuto doesn't support sixel. unlikely to use until it does
+        ${pkgs.pistol}/bin/pistol "$FILE_PATH" "$PREVIEW_WIDTH" "$PREVIEW_HEIGHT" && exit 0
+      '';
     };
   };
 }
