d945eb8e4ab6ee9d7acf10af5d5c87f7ad23dcdb
oneorseveralcats <oosc@noreply.codeberg.org>
2025-06-30 19:19:43 -0400

    nixos: add system-level testing module

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -14,6 +14,7 @@
     ./boot-amd64.nix
     ./college.nix
     ./rax.nix
+    ./testing.nix
   ];
 }
 
---
+++ nixos/modules/testing.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let 
+  cfg = config.myConfig.testing;
+in
+{
+  options.myConfig.testing = {
+    enable = lib.mkOption {
+      description = "Enable packages and modules that i am testing.";
+      type = types.bool;
+      default = true;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+    ];
+  };
+}
+
