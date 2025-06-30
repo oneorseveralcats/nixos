e141fed01c44890178cca7ea01a1ca63d4bbe489
oneorseveralcats <oosc@noreply.codeberg.org>
2025-06-30 19:18:27 -0400

    scheme: guile enable readline at startup

--- home-manager/modules/programming/scheme.nix
+++ home-manager/modules/programming/scheme.nix
@@ -12,6 +12,12 @@ in
     home.packages = with pkgs; [
       guile
     ];
+
+    home.file.".guile".text = ''
+      (use-modules (ice-9 readline))
+      (activate-readline)
+    '';
+      
   };
 }
 
