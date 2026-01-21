067dfa87394aeb2fe45d7a4ab77f9f8c663fae50
oneorseveralcats <oosc@noreply.codeberg.org>
2026-01-21 12:27:01 -0500

    base: add nmap

--- home-manager/modules/base.nix
+++ home-manager/modules/base.nix
@@ -59,7 +59,7 @@ in
       httrack
       imagemagickBig
       magic-wormhole megatools moreutils
-      ncdu
+      ncdu nmap
       pulsemixer python3Packages.yq
       rdrview ripgrep
       termdown
