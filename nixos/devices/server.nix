fab6353441dc371d18150da691e467186704d2fe
oneorseveralcats <oosc@noreply.codeberg.org>
2026-01-24 15:20:59 -0500

    nixos: add mounted file systems to server

--- nixos/devices/server.nix
+++ nixos/devices/server.nix
@@ -6,5 +6,16 @@
   myConfig.boot-amd64.enable = true;
   myConfig.connections.nas.enable = lib.mkForce false;
 
+  fileSystems."/media/jellyfin" = {
+    device = "/dev/disk/by-uuid/9f615694-c827-4bbc-90d2-53fd912f203f";
+    fsType = "ext4";
+  };
+
+  fileSystems."/media/nas" = {
+    device = "/dev/disk/by-uuid/cc4735c4-76c1-4612-95b9-335fb99b71e5";
+    fsType = "ext4";
+  };
+
+
   networking.hostName = "server";
 }
