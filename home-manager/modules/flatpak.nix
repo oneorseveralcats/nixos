01ade6bf7dc8124f6cf66bce7c183bb3c86ee823
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-03 00:17:54 -0500

    home-flatpak: add protontricks (seems more reliable than nixpkgs version)

--- home-manager/modules/flatpak.nix
+++ home-manager/modules/flatpak.nix
@@ -18,6 +18,7 @@ in
       uninstallUnmanaged = true;
       packages = [
         # "com.calibre_ebook.calibre" # until nixos fixes calibre tts: https://github.com/NixOS/nixpkgs/issues/364086
+        "com.github.Matoking.protontricks"
         "dev.vencord.Vesktop"
       ];
     };
