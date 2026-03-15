d76af2e1f3558767eff1d2e13480386f18ef752d
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-15 16:48:29 -0400

    meta: update generate-symlinks and move to /bin/

--- bin/generate-symlinks.sh
+++ bin/generate-symlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # used to generate the relevant symlinks for home-manager and nixos configs.
-dir=$(git rev-parse --show-toplevel)
+git_root=$(git rev-parse --show-toplevel)
 
 if [ -n "$XDG_CONFIG_HOME" ]; then
   cfg_dir="$XDG_CONFIG_HOME"
@@ -11,11 +11,11 @@ fi
 
 mkdir -p "$cfg_dir"
 
-ln -sf "$dir/.nix-channels" "$HOME/.nix-channels"
-ln -s "$dir/home-manager" "$cfg_dir/"
-ln -s "$dir/nixpkgs" "$cfg_dir/"
+ln -sf "$git_root/.nix-channels" "$HOME/.nix-channels"
+ln -s "$git_root/home-manager" "$cfg_dir/"
+ln -s "$git_root/nixpkgs" "$cfg_dir/"
 
-sudo ln -s "$dir/nixos" "/etc/"
+sudo ln -s "$git_root/nixos" "/etc/"
 
   
 
