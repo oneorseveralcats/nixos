1d6812362cbf6a45eadd719701bc05affd19b5d6
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-16 13:39:32 -0400

    nix-on-droid: use npins instead of channels

--- nixpkgs/nix-on-droid.nix
+++ nixpkgs/nix-on-droid.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }:
-{
+{ lib, pkgs, ... }:
+let
+  npinsPaths = lib.mapAttrsToList (k: v: "${k}=${v}") (import ../npins);
+in {
   android-integration = {
     termux-open.enable = true;
     termux-open-url.enable = true;
@@ -7,6 +9,8 @@
     termux-setup-storage.enable = true;
     xdg-open.enable = true;
   };
+
+  nix.nixPath = npinsPaths;
 
   home-manager.config = ../home-manager/home.nix;
   system.stateVersion = "21.11";
