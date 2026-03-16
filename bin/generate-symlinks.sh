3e14e14e7b1ec8c111eb22ca148f74437ca4ff7e
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-16 13:35:24 -0400

    bin: add .config/npins symlink

--- bin/generate-symlinks.sh
+++ bin/generate-symlinks.sh
@@ -15,6 +15,9 @@ ln -sf "$git_root/.nix-channels" "$HOME/.nix-channels"
 ln -s "$git_root/home-manager" "$cfg_dir/"
 ln -s "$git_root/nixpkgs" "$cfg_dir/"
 
+# nix-on-droid tries to look in .config/npins so this handles that
+ln -s "$git_root/npins" "$cfg_dir/"
+
 sudo ln -s "$git_root/nixos" "/etc/"
 
   
