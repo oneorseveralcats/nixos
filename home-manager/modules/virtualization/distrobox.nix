676cc864e5e4386078b13ef3bee36a8bd4f3ddfd
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-16 20:49:53 -0400

    home-distrobox: arch container only tries to install chaotic-aur once instead of every time the container starts

--- home-manager/modules/virtualization/distrobox.nix
+++ home-manager/modules/virtualization/distrobox.nix
@@ -25,20 +25,21 @@ in
             "paru"
           ];
           pre_init_hooks = let
-            chaotic-aur = pkgs.writers.writeBashBin "run.sh" ''
-                sudo su
-                pacman-key --init
-                pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-                pacman-key --lsign-key 3056513887B78AEB
-                pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'  
-                pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  
+            setup-chaotic-aur = pkgs.writers.writeBash "setup-chaotic-aur" ''
+                if [ ! -e "/etc/pacman.d/chaotic-mirrorlist" ]; then
+                  sudo su
+                  pacman-key --init
+                  pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+                  pacman-key --lsign-key 3056513887B78AEB
+                  pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'  
+                  pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  
 
-                printf '\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n' >> /etc/pacman.conf 
+                  printf '\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n' >> /etc/pacman.conf 
+                fi
               '';
           in [
-            "${chaotic-aur}/bin/run.sh"
-           
-            "pacman -Sy --noconfirm archlinux-keyring && pacman -Syu --noconfirm"
+            "${setup-chaotic-aur}"
+            "pacman -Sy --noconfirm archlinux-keyring"
             "ln -sf /usr/bin/vis /usr/bin/e"
           ];
         };
