7411807fb80d716415bba65cdec6c62a2df3efc0
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-17 12:10:16 +0000

    home-devices: nix-on-droid scripts to autostart ssh-agent and kill all child processes on exit.

--- home-manager/devices/nix-on-droid.nix
+++ home-manager/devices/nix-on-droid.nix
@@ -10,6 +10,21 @@
 
   home.shellAliases.home-manager = "nix-on-droid";
 
+  xdg.configFile = let
+    conf_d = "fish/conf.d";
+  in {
+    "${conf_d}/start-ssh-agent.fish".source = pkgs.writeScript "start-ssh-agent" ''
+      #!/usr/bin/env fish
+      if status --is-interactive && ! pgrep ssh-agent > /dev/null
+        eval (ssh-agent -c) > /dev/null
+      end
+    '';
+    "${conf_d}/kill-the-children.fish".source = pkgs.writeScript "kill-the-children" ''
+      #!/usr/bin/env fish
+      trap "trap - SIGTERM && kill -- -$fish_pid" SIGINT SIGTERM EXIT
+    '';
+  };
+
   programs.zellij.settings.default_shell = "bash";
 
   myHome.testing.enable = true;
