02bca450454f61889ce53c709513d57494d7164f
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-18 03:21:13 +0000

    home-devices: nix-on-droid: declaratively manage termux properties

--- home-manager/devices/nix-on-droid.nix
+++ home-manager/devices/nix-on-droid.nix
@@ -10,6 +10,11 @@
 
   home.shellAliases.home-manager = "nix-on-droid";
 
+  home.file.".termux/termux.properties".text = lib.generators.toKeyValue {} {
+    fullscreen = true;
+    # use-fullscreen-workaround = true;
+  };
+
   xdg.configFile."fish/conf.d/background-processes.fish".source = pkgs.writers.writeFish "background-processes" ''
     if status --is-interactive
       if ! pgrep ssh-agent > /dev/null
