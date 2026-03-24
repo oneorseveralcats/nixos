52c372348d88c07a676bd1c9a3e7f578581e1c25
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-24 19:31:13 -0400

    home-tridactyl: pre-set firefox gui settings to none so changes made after should be idempotent

--- home-manager/modules/browsers/tridactyl.nix
+++ home-manager/modules/browsers/tridactyl.nix
@@ -17,6 +17,7 @@ in
     xdg.configFile."tridactyl/tridactylrc".text = ''
       colorscheme shydactyl
 
+      guiset_quiet gui none
       guiset_quiet navbar autohide
       guiset_quiet tabs always
       guiset_quiet tabs count
