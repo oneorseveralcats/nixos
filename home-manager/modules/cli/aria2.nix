68b2fbcf8f45731a307ff84415091dbcf9fe2b70
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-26 16:59:33 -0400

    home-aria2: set download directory, remove "with lib;"

--- home-manager/modules/cli/aria2.nix
+++ home-manager/modules/cli/aria2.nix
@@ -1,14 +1,13 @@
 { config, lib, pkgs, ... }:
-with lib;
 let 
   cfg = config.myHome.cli.aria2;
 in
 {
   options.myHome.cli.aria2 = {
-    enable = lib.mkEnableOption "Enable the aria2 download manager.";
+    enable = lib.mkEnableOption "the aria2 download manager.";
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     home.packages = [
       pkgs.python3Packages.aria2p
     ];
@@ -16,6 +15,7 @@ in
     programs.aria2 = {
       enable = true;
       settings = {
+        dir = "${config.xdg.userDirs.download}";
         enable-rpc = true;
         rpc-listen-all = false;
       };
