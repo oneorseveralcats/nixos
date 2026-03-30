e942957b4c3f84c7bc5bbaab098d9af8a4533b88
oneorseveralcats <oosc@noreply.codeberg.org>
2026-03-30 11:10:21 -0400

    home-aria2: add systemd service

--- home-manager/modules/cli/aria2.nix
+++ home-manager/modules/cli/aria2.nix
@@ -14,10 +14,34 @@ in
 
     programs.aria2 = {
       enable = true;
-      settings = {
+      settings = let
+        aria2Dir = "${config.xdg.configHome}/aria2";
+      in{
         dir = "${config.xdg.userDirs.download}";
         enable-rpc = true;
         rpc-listen-all = false;
+        save-session = "${aria2Dir}/session.txt";
+        save-session-interval = 300;
+        # process crashes if input file isn't there.
+        input-file = "${aria2Dir}/session.txt";
+      };
+    };
+
+    systemd.user.services.aria2c = {
+      Unit = {
+        Description = "Aria2c daemon";
+        Documentation = "man:aria2(1)";
+        # when upstreamed
+        # X-Restart-Triggers = lib.mkIf (cfg.settings != { }) [ "${config.xdg.configFile."aria2/aria2.conf".source}" ];
+        X-Restart-Triggers = [ "${config.xdg.configFile."aria2/aria2.conf".source}" ];
+
+      };
+      Install.WantedBy = [ "default.target" ];
+      Service = {
+        # upstream option
+        # ExecStart = "${lib.getExe cfg.package} --enable-rpc";
+        ExecStart = "${lib.getExe config.programs.aria2.package} --enable-rpc";
+        Restart = "on-failure";
       };
     };
   };
