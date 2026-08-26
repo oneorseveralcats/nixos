24193770b212a3f5f2167c3af90a62b2a7ed6d44
oneorseveralcats <oosc@noreply.codeberg.org>
2026-08-26 19:42:32 -0400

    meta: add a default.nix that generates the most up-to-date NIX_PATH from npins sources

--- default.nix
+++ default.nix
@@ -3,10 +3,10 @@ let
   lib = pkgs.lib;
   nixPath = lib.concatStringsSep " " (
     lib.mapAttrsToList (k: v: lib.optionalString (lib.isAttrs v) "${k}=${v}")
-    (import ./default.nix)
+    (import ./npins/default.nix)
   );
 in pkgs.mkShell {
   shellHook = ''
-    export NIX_PATH="${nixPath}"
+    export NIX_PATH="nixos-config=$HOME/git/nixos/nixos/configuration.nix ${nixPath}"
   ''; 
 }
