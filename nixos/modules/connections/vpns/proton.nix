d596ec4cf1f461b6179801c662d80aa6de49470d
oneorseveralcats <oosc@noreply.codeberg.org>
2026-09-20 19:06:05 -0400

    nixos-proton: rename proton-vpn package

--- nixos/modules/connections/vpns/proton.nix
+++ nixos/modules/connections/vpns/proton.nix
@@ -10,7 +10,7 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      proton-vpn-cli protonvpn-gui
+      proton-vpn-cli proton-vpn
       wireguard-tools
     ];
 
