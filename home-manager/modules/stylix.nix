7ad10bd60627b6fd72f98791a052f23cf3aede85
oneorseveralcats <oosc@noreply.codeberg.org>
2026-09-20 14:33:48 -0400

    home-stylix: switch base16Scheme, some remove commented code

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -37,27 +37,27 @@ in
       enable = true;
       polarity = "dark";
       image = "${pkgs.nixos-artwork.wallpapers.binary-black}/share/backgrounds/nixos/nix-wallpaper-binary-black.png";
-      # base16Scheme = "${pkgs.base16-schemes}/share/themes/evenok-dark.yaml";
-      base16Scheme = {
-        name = "dark and vibrant";
-        base00 = "#000000";
-        base01 = "#202020";
-        base02 = "#404040";
-        base03 = "#606060";
-        base04 = "#b0b0b0";
-        base05 = "#d0d0d0";
-        base06 = "#e0e0e0";
-        base07 = "#ffffff";
-        # base08 = "#f5708a";
-        base08 = "#ee2244";
-        base09 = "#ee8122";
-        base0A = "#b8a300";
-        base0B = "#54bc5c";
-        base0C = "#00bab3";
-        base0D = "#00aff2";
-        base0E = "#9095ff";
-        base0F = "#d47ada";
-      };
+      base16Scheme = "${pkgs.unstable.base16-schemes}/share/themes/irblack.yaml";
+      # base16Scheme = {
+      #   name = "dark and vibrant";
+      #   base00 = "#000000";
+      #   base01 = "#202020";
+      #   base02 = "#404040";
+      #   base03 = "#606060";
+      #   base04 = "#b0b0b0";
+      #   base05 = "#d0d0d0";
+      #   base06 = "#e0e0e0";
+      #   base07 = "#ffffff";
+      #   # base08 = "#f5708a";
+      #   base08 = "#ee2244";
+      #   base09 = "#ee8122";
+      #   base0A = "#b8a300";
+      #   base0B = "#54bc5c";
+      #   base0C = "#00bab3";
+      #   base0D = "#00aff2";
+      #   base0E = "#9095ff";
+      #   base0F = "#d47ada";
+      # };
       
       cursor = {
         name = "phinger-cursors-dark";
@@ -78,10 +78,6 @@ in
           name = "Noto Sans Light";
         };
         serif = sansSerif;
-        # serif = {
-        #   package = sansSerif.package;
-        #   name = sansSerif.name;
-        # };
         monospace = {
           package = pkgs.nerd-fonts.fira-code;
           name = "FiraCode Nerd Font Light";
